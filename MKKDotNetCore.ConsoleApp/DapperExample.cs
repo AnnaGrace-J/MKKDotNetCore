29e53041cd6ec8a43e3fe824eb50a9d7cda67884
AnnaGrace-J <maykyawtkhaing2842@gmail.com>
2026-06-26 21:32:13 +0630

    feat: add getById Method

--- MKKDotNetCore.ConsoleApp/DapperExample.cs
+++ MKKDotNetCore.ConsoleApp/DapperExample.cs
@@ -99,6 +99,43 @@ namespace MKKDotNetCore.ConsoleApp
                 });
                 Console.WriteLine(value: result == 1 ? "Delete successful" : "Delete failed");
             }
+
+
         }
+
+        public void Edit(int id)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                string query = "select * from Tbl_blog where DeleteFlag=0 and BlogId = @BlogId;";
+                //var List<dynamic>? lst= db.Query(sql: query,).ToList();
+                //foreach(var dynamic item in lst){
+                //    Console.WriteLine(item.BlogId);
+                //    Console.WriteLine(item.BlogTitle);
+                //    Console.WriteLine(item.BlogAuthor);
+                //    Console.WriteLine(item.BlogContent);
+
+                var item =  db.Query<BlogDataModel>(sql: query, param: new BlogDataModel
+                {
+                    BlogId = id,
+                }).FirstOrDefault();
+
+                if(item is null)
+                {
+                    Console.WriteLine(value: "No data found");
+                    return;
+                }
+                
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogAuthor);
+                    Console.WriteLine(item.BlogContent);
+                
+            }
+
+            //DTO ==> Data Transfer Object
+
+        }
+
     }
 }
